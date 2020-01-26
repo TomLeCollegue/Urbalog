cc1e739a494bb88cda5e1fc7c1f6c35ba415b097
Laurent Cutting <laurent.cutting@gmail.com>
2020-01-26 15:15:24 +0100

    Admin Interface added -> activities landscape mode

---
+++ app/src/main/java/com/example/urbalog/AdminInterfaceConnection.java
@@ -0,0 +1,14 @@
+package com.example.urbalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminInterfaceConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_interface_connection);
+    }
+}
