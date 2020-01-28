7cf8a9b13efaa30493bbfa00c6745aeb8905d011
Hugo Hersemeule <owydoo@gmail.com>
2020-01-28 09:42:27 +0100

    player_connexion_activity vue
    Vue de la connexion pour le joueur

--- app/src/main/java/com/example/urbalog/playerConnexionActivity.java
+++ app/src/main/java/com/example/urbalog/playerConnexionActivity.java
@@ -1,4 +1,4 @@
-package com.example.urbalog;
+package com.example.urbalog_login_joueur_interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player_connexion);
     }
 }
