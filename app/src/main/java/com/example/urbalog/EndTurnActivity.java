be856fde9821f4f8df35d4783692dfc3a2645da8
Hugo Hersemeule <owydoo@gmail.com>
2020-03-01 18:20:46 +0100

    Working on EndTurnActivity

---
+++ app/src/main/java/com/example/urbalog/EndTurnActivity.java
@@ -0,0 +1,14 @@
+package com.example.urbalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndTurnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_turn);
+    }
+}
