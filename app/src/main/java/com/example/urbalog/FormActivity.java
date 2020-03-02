7e84126dd94e4ac374b2c8154a13e53e0800e8d7
Samuel <samuel310597@gmail.com>
2020-03-02 22:40:28 +0100

    Add new activity for form
    implement pageview system for form

---
+++ app/src/main/java/com/example/urbalog/FormActivity.java
@@ -0,0 +1,74 @@
+package com.example.urbalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.urbalog.Adapter.FormAdapter;
+
+public class FormActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotsLayout;
+
+    private TextView[] mDots;
+
+    private FormAdapter formAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotsLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        formAdapter = new FormAdapter(this);
+        mSlideViewPager.setAdapter(formAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+    }
+
+    public void addDotsIndicator(int position){
+        mDots = new TextView[3];
+        mDotsLayout.removeAllViews();
+
+        for (int i=0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.blue));
+
+            mDotsLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorDelete));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
