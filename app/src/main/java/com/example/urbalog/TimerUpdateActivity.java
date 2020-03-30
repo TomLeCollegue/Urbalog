b8844d216ecb4dba048c94d0ca1d1eead0beb20f
laurent cutting <laurent.cutting@gmail.com>
2020-03-30 16:15:19 +0200

    files added, update timer.class and .xml -> need to setup button

---
+++ app/src/main/java/com/example/urbalog/TimerUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.urbalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimerUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_update);
+    }
+}
