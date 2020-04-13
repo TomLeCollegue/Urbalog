2527cc0d6603c1183213fec9284656173c880120
laurent cutting <laurent.cutting@gmail.com>
2020-04-13 03:11:14 +0200

    settings view update, intermediate buttons

---
+++ app/src/main/java/com/example/urbalog/BuildingConfigActivity.java
@@ -0,0 +1,62 @@
+package com.example.urbalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.urbalog.Class.Building;
+import com.example.urbalog.Json.JsonBuilding;
+
+public class BuildingConfigActivity extends AppCompatActivity {
+
+    private Button modificationBuilding;
+    private Button initBuilding;
+    private Button addBuilding;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_config);
+
+        modificationBuilding = findViewById(R.id.modificationBuilding);
+
+        modificationBuilding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BuildingConfigActivity.this, ListBuildingsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        initBuilding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsonBuilding.recreate(mContext);
+                Toast.makeText(mContext, "Les batiments on été réinitialisé", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        addBuilding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BuildingConfigActivity.this, AddBuildingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void onBackPressed(){
+        Intent intent = new Intent(BuildingConfigActivity.this, ConfigurationActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+    }
+}
--- app/src/main/java/com/example/urbalog/ConfigurationActivity.java
+++ app/src/main/java/com/example/urbalog/ConfigurationActivity.java
@@ -18,15 +18,12 @@ import java.util.ArrayList;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
-    private Button modificationBuilding;
-    private Button initBuilding;
-    private Button addBuilding;
     private Button statistics;
-    private Button addGame;
-    private Button initStats;
     private Button resetDb;
     private Button exportCsv;
     private Button updateTimer;
+    private Button buildingConfig;
+    private Button dbConfig;
     private Context mContext;
 
     @Override
@@ -36,41 +33,13 @@ public class ConfigurationActivity extends AppCompatActivity {
 
         mContext = ConfigurationActivity.this;
 
-        modificationBuilding = findViewById(R.id.modificationBuilding);
-        initBuilding = findViewById(R.id.initBuilding);
-        addBuilding = findViewById(R.id.addBuilding);
         statistics = findViewById(R.id.statistics);
-        addGame = findViewById(R.id.addGame);
-        initStats = findViewById(R.id.initStats);
         resetDb = findViewById(R.id.resetDb);
         exportCsv = findViewById(R.id.exportCsv);
         updateTimer = findViewById(R.id.updateTimer);
+        buildingConfig = findViewById(R.id.buildingButton);
+        dbConfig = findViewById(R.id.dbButton);
 
-        modificationBuilding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ConfigurationActivity.this, ListBuildingsActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        initBuilding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JsonBuilding.recreate(mContext);
-                Toast.makeText(mContext, "Les batiments on été réinitialisé", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        addBuilding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ConfigurationActivity.this, AddBuildingActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
 
         statistics.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,58 +50,30 @@ public class ConfigurationActivity extends AppCompatActivity {
             }
         });
 
-        addGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JsonStats.giveContext(ConfigurationActivity.this);
-                ArrayList<Player> list = new ArrayList<Player>();
-                Player un = new Player("Gab", 20, "Retraité", null);
-                list.add(un);
-                Player deux = new Player("Raph", 22, "Agriculteur, exploitant", null);
-                list.add(deux);
-                Player trois = new Player("Leo", 24, "Ouvrier qualifié", null);
-                list.add(trois);
-                Player quatre = new Player("Louis", 25, "Cadres et profession intellectuelle supérieure", null);
-                list.add(quatre);
-                Player cinq = new Player("Lucas", 26, "Sans emploi", null);
-                list.add(cinq);
-                Game game = new Game();
-                game.setScoreAttractivite(2);
-                game.setScoreFluidite(1);
-                game.setScoreEnvironnemental(-4);
-                JsonStats.writeGame(list, game);
-                Toast.makeText(ConfigurationActivity.this, "partie ajoutée", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        initStats.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JsonStats.init(ConfigurationActivity.this);
-                Toast.makeText(ConfigurationActivity.this, "Stats réinitialisés", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        resetDb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AdminConnectionActivity.net.getDb().resetDB();
-                Toast.makeText(ConfigurationActivity.this, "Base de donnée réinitialisée", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        exportCsv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AdminConnectionActivity.net.getDb().exportDbToCSV();
-                Toast.makeText(ConfigurationActivity.this, "Base de donnée exportée", Toast.LENGTH_LONG).show();
-            }
-        });
+        
 
         updateTimer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ConfigurationActivity.this, TimerUpdateActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buildingConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfigurationActivity.this, BuildingConfigActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        dbConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfigurationActivity.this, DbConfigActivity.class);
                 startActivity(intent);
                 finish();
             }
